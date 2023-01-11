ca191f68795eadbc8123fc2ec6a008413b05ff5d
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-01-11 12:49:47 +0100

    Changed to max_new_tokens

--- server/codegen.py
+++ server/codegen.py
@@ -5,5 +5,5 @@ model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-2B-mono").to(0)
 
 def get_completion(context):
     inputs = tokenizer(context, return_tensors="pt").to(0)
-    sample = model.generate(**inputs, max_length=512)
+    sample = model.generate(**inputs, max_new_tokens=500)
     return tokenizer.decode(sample[0], truncate_before_pattern=["\n\n^#", "^'''", "\n\n\n"])
--- server/polycoder.py
+++ server/polycoder.py
@@ -1,9 +1,9 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("NinedayWang/PolyCoder-2.7B")
-model = AutoModelForCausalLM.from_pretrained("NinedayWang/PolyCoder-2.7B")
+model = AutoModelForCausalLM.from_pretrained("NinedayWang/PolyCoder-2.7B").to(0)
 
 def get_completion(context):
     inputs = tokenizer.encode(context, return_tensors="pt")
-    sample = model.generate(inputs, max_length=2048, num_beams=4, num_return_sequences=1)
+    sample = model.generate(inputs, max_new_tokens=500, num_beams=4, num_return_sequences=1)
     return tokenizer.decode(sample[0], truncate_before_pattern=["\n\n^#", "^'''", "\n\n\n"])
