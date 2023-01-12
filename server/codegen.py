d0533a42a3cddbd2d4a5f87390e1c4efbac23f33
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-01-12 11:38:00 +0100

    Updated encoding of tokenizer

--- server/codegen.py
+++ server/codegen.py
@@ -4,6 +4,6 @@ tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-350M-mono")
 model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono").to(0)
 
 def get_completion(context):
-    inputs = tokenizer(context, return_tensors="pt").to(0)
-    sample = model.generate(**inputs, max_new_tokens=500)
+    inputs = tokenizer.encode(context, return_tensors="pt").to(0)
+    sample = model.generate(inputs, max_new_tokens=500)
     return tokenizer.decode(sample[0], truncate_before_pattern=["\n\n\n", "def ", "class "])
