87b9d5d74500bc4ace2cb711a6382cb9cefe88c3
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-01-12 11:36:36 +0100

    Unified stop tokens

--- server/codegen.py
+++ server/codegen.py
@@ -6,4 +6,4 @@ model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-350M-mono").to(
 def get_completion(context):
     inputs = tokenizer(context, return_tensors="pt").to(0)
     sample = model.generate(**inputs, max_new_tokens=500)
-    return tokenizer.decode(sample[0], truncate_before_pattern=["\n\n^#", "^'''", "\n\n\n"])
+    return tokenizer.decode(sample[0], truncate_before_pattern=["\n\n\n", "def ", "class "])
--- server/polycoder.py
+++ server/polycoder.py
@@ -6,4 +6,4 @@ model = AutoModelForCausalLM.from_pretrained("NinedayWang/PolyCoder-0.4B").to(0)
 def get_completion(context):
     inputs = tokenizer.encode(context, return_tensors="pt").to(0)
     sample = model.generate(inputs, max_new_tokens=500, num_beams=4, num_return_sequences=1)
-    return tokenizer.decode(sample[0], truncate_before_pattern=["\n\n^#", "^'''", "\n\n\n"])
+    return tokenizer.decode(sample[0], truncate_before_pattern=["\n\n\n", "def ", "class "])
