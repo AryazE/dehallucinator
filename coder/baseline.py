5dfdab35dfb897ab296de850d37c7375c81284fb
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-02-25 20:20:38 +0100

    Fixed baseline

--- coder/baseline.py
+++ coder/baseline.py
@@ -2,6 +2,6 @@ from .utils import get_completion_safely, get_indentation, postprocess
 
 def completion(model, completor, prompt):
     indent_style, indent_count = get_indentation(prompt)
-    comp = get_completion_safely(model, completor, '', prompt)
+    comp = get_completion_safely(model, completor, prompt)
     comp = postprocess(comp, indent_style, indent_count)
     return prompt, comp
