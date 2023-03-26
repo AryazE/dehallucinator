c31453b497f9ee1fa55d64615240d28ebb14092c
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-03-27 00:51:48 +0200

    Added 2 repos

---
+++ benchmark/run_8_ablation.sh
@@ -0,0 +1,26 @@
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/psf_black.json --mode baselinek6 --log 1 --k 6
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/geopy_geopy.json --mode baselinek6 --log 1 --k 6
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/nvbn_thefuck.json --mode baselinek6 --log 1 --k 6
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/graphql-python_graphene.json --mode baselinek6 --log 1 --k 6
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/arrow-py_arrow.json --mode baselinek6 --log 1 --k 6
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/wtforms_wtforms.json --mode baselinek6 --log 1 --k 6
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/rspeer_python-ftfy.json --mode baselinek6 --log 1 --k 6
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/Alir3z4_html2text.json --mode baselinek6 --log 1 --k 6
+
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/psf_black.json --mode docstringt4 --log 1 --k 3 --t 0.4
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/geopy_geopy.json --mode docstringt4 --log 1 --k 3 --t 0.4
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/nvbn_thefuck.json --mode docstringt4 --log 1 --k 3 --t 0.4
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/graphql-python_graphene.json --mode docstringt4 --log 1 --k 3 --t 0.4
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/arrow-py_arrow.json --mode docstringt4 --log 1 --k 3 --t 0.4
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/wtforms_wtforms.json --mode docstringt4 --log 1 --k 3 --t 0.4
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/rspeer_python-ftfy.json --mode docstringt4 --log 1 --k 3 --t 0.4
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/Alir3z4_html2text.json --mode docstringt4 --log 1 --k 3 --t 0.4
+
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/psf_black.json --mode docstringc2 --log 1 --k 3 --c 2
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/geopy_geopy.json --mode docstringc2 --log 1 --k 3 --c 2
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/nvbn_thefuck.json --mode docstringc2 --log 1 --k 3 --c 2
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/graphql-python_graphene.json --mode docstringc2 --log 1 --k 3 --c 2
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/arrow-py_arrow.json --mode docstringc2 --log 1 --k 3 --c 2
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/wtforms_wtforms.json --mode docstringc2 --log 1 --k 3 --c 2
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/rspeer_python-ftfy.json --mode docstringc2 --log 1 --k 3 --c 2
+time python benchmark/run_project.py --model GPT3.5 --config benchmark/benchmark_configs/Alir3z4_html2text.json --mode docstringc2 --log 1 --k 3 --c 2
