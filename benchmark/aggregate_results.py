742821e11c8529057bd59a09f41b71951c9f66d0
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-03-27 13:07:11 +0200

    Fixed minor issue

--- benchmark/aggregate_results.py
+++ benchmark/aggregate_results.py
@@ -8,15 +8,23 @@ if __name__ == '__main__':
     args = parser.parse_args()
     csv_files = Path(__file__).resolve().parent.glob(args.glob)
     results = []
+    max_fails = []
+    ind = 0
+    jj = []
     for file in csv_files:
         with open(file, 'r') as f:
             lines = f.read().splitlines()
+        max_f = 0
         for line in lines[1:]:
             results.append([float(i) for i in line.split(',')[1:]])
+            max_f = max(max_f, int(results[-1][0]), int(results[-1][3]))
+            jj.append(ind)
+        max_fails.append(max_f)
+        ind += 1
     headers = lines[0].split(',')[1:]
     cols = []
     for i in range(len(results[0])):
-        cols.append([results[j][i] for j in range(len(results))])
+        cols.append([results[j][i] if results[j][i] > -1 else max_fails[jj[j]] for j in range(len(results))])
     with open('aggregate.csv', 'w') as f:
         print('col, min, mean, max, std')
         f.write('col, min, mean, max, std\n')
