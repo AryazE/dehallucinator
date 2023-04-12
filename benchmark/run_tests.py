3b834fabcda4fe11f2ade8906d6e30900269ac7b
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-04-12 14:31:43 +0200

    Fixed test selection

--- benchmark/run_tests.py
+++ benchmark/run_tests.py
@@ -77,7 +77,7 @@ def run_tests(config: Dict[str, Any], id: int, mode: str, executable: str) -> Li
             tests = []
             for t in tests_per_line[line]:
                 parts = t.split('.')
-                tests.append(f'{("/".join(parts[:-3]) + "/") if len(parts) > 3 else ""}{parts[-3]}.py::{parts[-2]}::{parts[-1]}')
+                tests.append(f'{str(temp_dir/config["project_root"])}/{("/".join(parts[:-3]) + "/") if len(parts) > 3 else ""}{parts[-3]}.py::{parts[-2]}::{parts[-1]}')
             pytest_command += ['-v'] + tests
         pytest_command.append(str(temp_dir/config['project_root']/config['tests_path']))
         try:
