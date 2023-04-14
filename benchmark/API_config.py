3a4d73e17df7099ed6644b2277f716ac3436654d
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-04-14 14:39:27 +0200

    Benchmarking for line and API completions

---
+++ benchmark/API_config.py
@@ -0,0 +1,99 @@
+from typing import Optional
+import argparse
+from pathlib import Path
+import libcst as cst
+import libcst.matchers as m
+from typing import NamedTuple
+import json
+
+FunctionInfo = NamedTuple('FunctionInfo', [('function', str), ('start_line', int), ('start_column', int), ('end_line', int), ('end_column', int)])
+
+class APIFinder(cst.CSTVisitor):
+    METADATA_DEPENDENCIES = (cst.metadata.PositionProvider, cst.metadata.QualifiedNameProvider,)
+    def __init__(self):
+        self.function = ['']
+        self.apis = []
+
+    def visit_FunctionDef(self, node):
+        self.function.append(node.name.value)
+    
+    def leave_FunctionDef(self, node):
+        self.function.pop()
+
+    def visit_Call(self, node: cst.Call) -> Optional[bool]:
+        qname = self.get_metadata(cst.metadata.QualifiedNameProvider, node)
+        if qname is cst.metadata.QualifiedNameSource.BUILTIN:
+            return False
+        pos = self.get_metadata(cst.metadata.PositionProvider, node)
+        start = pos.start
+        end = pos.end
+        self.apis.append(FunctionInfo(self.function[-1], start.line, start.column, end.line, end.column))
+        return False
+
+def extract_evaluations(file):
+    with open(file, 'r') as f:
+        code = f.read()
+    wrapper = cst.metadata.MetadataWrapper(cst.parse_module(code))
+    finder = APIFinder()
+    wrapper.visit(finder)
+    return finder.apis
+
+
+def make_config(project, tests):
+    project_path = Path(project).resolve()
+    tests_path = Path(tests).resolve()
+    files_to_ignore = set(tests_path.glob('**/*.py'))
+    files_to_ignore.add(project_path/'setup.py')
+    files_to_ignore.add(project_path/'__init__.py')
+    files_to_ignore.update(project_path.glob('**/__init__.py'))
+    files_to_ignore.update(project_path.glob('.*/**/*.py'))
+    files_to_ignore.update(project_path.glob('docs*/**/*.py'))
+    files_to_ignore.update(project_path.glob('examples*/**/*.py'))
+    python_files = [f for f in project_path.glob('**/*.py') if f not in files_to_ignore]
+    evaluations = [{
+        'id': 0,
+        'file': '',
+        'function': '',
+        'remove': []
+    }]
+    id = 1
+    print(len(python_files))
+    for f in python_files:
+        try:
+            e = extract_evaluations(f)
+        except:
+            continue
+        for i in e:
+            if i.function == '__init__':
+                continue
+            evaluations.append({
+                'id': id, 
+                'file': f.relative_to(project_path).as_posix(), 
+                'function': i.function, 
+                'remove': [{
+                    'description': 'code',
+                    'start_line': i.start_line,
+                    'start_column': i.start_column,
+                    'end_line': i.end_line,
+                    'end_column': i.end_column + 1
+                }]
+            })
+            id += 1
+            print('-', end='', flush=True)
+    project_name = project_path.as_posix().split('/')[-1]
+    benchmark_dir = Path(__file__).resolve().parent
+    config = {
+        'name': project_name,
+        'project_root': project_path.as_posix().lstrip(benchmark_dir.as_posix()),
+        'tests_path': tests[len(project):].strip('/'),
+        'evaluations': evaluations
+    }
+    with open(Path(__file__).parent/'benchmark_configs'/f'{project_name}.json', 'w') as f:
+        json.dump(config, f, indent=4)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--project', type=str, required=True)
+    parser.add_argument('--tests', type=str, required=True)
+    args = parser.parse_args()
+    make_config(args.project, args.tests)
---
+++ benchmark/config_to_lines.py
@@ -0,0 +1,27 @@
+import argparse
+import json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, required=True)
+    args = parser.parse_args()
+    with open(args.config) as f:
+        config = json.load(f)
+    new_config = config.deepcopy()
+    new_config['evaluations'] = []
+    for e in config['evaluations']:
+        if e['id'] == 0:
+            new_config['evaluations'].append(e)
+            continue
+        for i in range(e['start_line'], e['end_line'] + 1):
+            new_config['evaluations'].append({
+                'id': e['id'],
+                'file': e['file'],
+                'function': e['function'],
+                'start_line': i,
+                'end_line': i+1,
+                'start_column': e['start_column'],
+                'end_column': 1
+            })
+    with open(args.config[:-5] + '-lines.json', 'w') as f:
+        json.dump(new_config, f, indent=2)
