9a1ec6f54e59d565452eb242a2fb7219271e140e
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-04-16 10:24:09 +0200

    Balanced function representation

--- benchmark/config_to_lines.py
+++ benchmark/config_to_lines.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 from copy import deepcopy
+from random import random
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -17,18 +18,20 @@ if __name__ == '__main__':
             continue
         rem = e['remove'][0]
         for i in range(rem['start_line'], rem['end_line'] + 1):
-            new_config['evaluations'].append({
-                'id': j,
-                'file': e['file'],
-                'function': e['function'],
-                'remove': [{
-                    'description': 'code',
-                    'start_line': i,
-                    'end_line': i+1,
-                    'start_column': rem['start_column'],
-                    'end_column': 1
-                }]
-            })
-            j += 1
+            x = random()
+            if x < 1.0/(rem['end_line'] - rem['start_line'] + 1):
+                new_config['evaluations'].append({
+                    'id': j,
+                    'file': e['file'],
+                    'function': e['function'],
+                    'remove': [{
+                        'description': 'code',
+                        'start_line': i,
+                        'end_line': i+1,
+                        'start_column': rem['start_column'],
+                        'end_column': 1
+                    }]
+                })
+                j += 1
     with open(args.config[:-5] + '-lines.json', 'w') as f:
         json.dump(new_config, f, indent=2)
