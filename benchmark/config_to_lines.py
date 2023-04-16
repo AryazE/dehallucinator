3114ffb3425f8af86f6aa65c13e18e9f529ba4f6
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-04-16 10:11:18 +0200

    Deepcopy

--- benchmark/config_to_lines.py
+++ benchmark/config_to_lines.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from copy import deepcopy
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -7,10 +8,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     with open(args.config) as f:
         config = json.load(f)
-    new_config = config
-    copy_evals = config['evaluations'].deepcopy()
+    new_config = deepcopy(config)
     new_config['evaluations'] = []
-    for e in copy_evals:
+    for e in config['evaluations']:
         if e['id'] == 0:
             new_config['evaluations'].append(e)
             continue
