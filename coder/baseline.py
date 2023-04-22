a6c96568974b13e8837ef1ac5f43283f3c8b0856
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-04-22 11:19:17 +0200

    Fixed baseline postprocessing

--- coder/baseline.py
+++ coder/baseline.py
@@ -1,6 +1,6 @@
 from .utils import get_completion_safely, get_indentation, postprocess
 
-def completion(model, completor, prompt, k=4):
+def completion(model, completor, prompt, k=4, mode=''):
     indent_style, indent_count = get_indentation(prompt)
     comp = get_completion_safely(model, completor, prompt, k=1)
     tmp = get_completion_safely(model, completor, prompt, k=k)
@@ -8,5 +8,5 @@ def completion(model, completor, prompt, k=4):
         comp = tmp
     else:
         comp.extend(tmp[:-1])
-    comp = [postprocess(i, indent_style, indent_count) for i in comp]
+    comp = [postprocess(i, indent_style, indent_count, mode) for i in comp]
     return prompt, comp
