f3d5d11f1b76bb234c114158de78359b3b42adf5
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2023-10-13 16:08:24 +0200

    Scripts

---
+++ benchmark/all_tok_dist.sh
@@ -0,0 +1,8 @@
+for llm in "CodeGen25"; do
+        for c in "2" "10" "20" "40"; do
+                echo ${llm}_${c}---------------------------------------------------
+                for project in "aaugustin_websockets" "arrow-py_arrow" "geopy_geopy" "graphql-python_graphene" "lektor_lektor" "mwaskom_seaborn" "nvbn_thefuck" "psf_black" "scikit-learn_scikit-learn" "Parsely_streamparse" "Supervisor_supervisor"; do
+                        python benchmark/token_distance.py --results res_${project}_${llm}c${c}k5t01testsapi
+                done
+        done
+done
---
+++ benchmark/run_var.sh
@@ -0,0 +1,32 @@
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/aaugustin_websockets-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/aaugustin_websockets-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/arrow-py_arrow-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/arrow-py_arrow-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/geopy_geopy-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/geopy_geopy-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/graphql-python_graphene-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/graphql-python_graphene-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/lektor_lektor-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/lektor_lektor-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/mwaskom_seaborn-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/mwaskom_seaborn-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/nvbn_thefuck-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/nvbn_thefuck-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/Parsely_streamparse-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/Parsely_streamparse-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/psf_black-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/psf_black-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/scikit-learn_scikit-learn-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/scikit-learn_scikit-learn-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
+
+#time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/Supervisor_supervisor-apis.json --mode commentCodeGen25c40k5t01testsapi --log 1 --k 5 --c 40 --t 0.1 --noTests
+time python benchmark/run_project.py --model lCodeGen25 --config benchmark/benchmark_configs/Supervisor_supervisor-apis.json --mode commentCodeGen25c2k5t01testsapi --log 1 --k 5 --c 2 --t 0.1 --noTests
